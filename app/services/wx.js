6a911fa1e2b8eb14e169c62ae9fd290473c8c709
lyllovelemon <732193462@qq.com>
2019-12-05 21:30:39 +0800

    feat:add mini_program loginType

--- app/services/wx.js
+++ app/services/wx.js
@@ -1,6 +1,18 @@
+const util=require('util')
+const axios=require('axios')
 class WxManager {
     static async codeToToken(code){
         //需要 code appId appSecret三个参数
-        
+        const url=util.format(global.config.wx.loginUrl,global.config.wx.appId,global.config.wx.appSecret,
+            code)
+        const result=await axios.get(url)
+        if(result.status!==200){
+            throw new global.errs.AuthFailed('openId获取失败')
+        }
+        const errCode=result.data.errcode
+        if(errCode!==0){
+            throw new global.errs.AuthFailed('openId获取失败'+errCode)
+        }
+
     }
 }
